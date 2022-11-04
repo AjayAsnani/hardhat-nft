1a9e641e765877bc4e71df68d3a4b4a6ef8a9b22
AjayAsnani <101942594+AjayAsnani@users.noreply.github.com>
2022-11-04 22:43:10 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,18 +10,20 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKET_API = process.env.COINMARKET_API
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 
 module.exports = {
     solidity: {
         compilers: [
             { version: "0.8.8" },
+            { version: "0.8.7" },
             { version: "0.6.6" },
             { version: "0.6.12" },
             { version: "0.4.19" },
         ],
     },
     defaultNetwork: "hardhat",
-    network: {
+    networks: {
         hardhat: {
             chainId: 31337,
             forking: {
@@ -41,16 +43,17 @@ module.exports = {
             coinmarketcap: COINMARKET_API,
             token: "MATIC",
         },
-        etherscan: {
-            apikey: ETHERSCAN_API_KEY,
+    },
+    etherscan: {
+        apikey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
         },
-        namedAccounts: {
-            deployer: {
-                default: 0,
-            },
-            user: {
-                default: 1,
-            },
+        user: {
+            default: 1,
         },
     },
 }
+
